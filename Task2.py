72420e85a0a37a5c0e13ab07ff3fcfdfa1aa3446
Pemberai Sweto <pembeweb@yahoo.com>
2020-01-25 22:02:22 +0200

    feat: add solution for Task2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,3 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files
-"""
 import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -19,4 +15,13 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
-
+telephone_numbers = dict()
+for i in range(len(calls)):
+    telephone_numbers[calls[i][0]] = telephone_numbers.get(
+        calls[i][0], 0) + int(calls[i][-1])
+    telephone_numbers[calls[i][1]] = telephone_numbers.get(
+        calls[i][1], 0) + int(calls[i][-1])
+number_with_max_time = sorted(
+    telephone_numbers, reverse=True, key=lambda x: telephone_numbers[x])[0]
+print(str(number_with_max_time) + " spent the longest time, " +
+      str(telephone_numbers[number_with_max_time]) + " seconds, on the phone during September 2016.")
